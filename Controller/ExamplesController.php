b4e81d9a2cdb5fe04e108a28c12a42a59290377a
Cosmin-Mihai Tutunaru <ctutunaru@pitechnologies.ro>
2013-06-25 15:51:54 +0300

    Remove unwanted comments from ExamplesController.php.

--- Controller/ExamplesController.php
+++ Controller/ExamplesController.php
@@ -28,9 +28,9 @@ class ExamplesController extends Controller
 {
   /**
    * ExamplesController::indexAction()
-   * 
+   *
    * Retrieve a list of available pChart examples for the user to browse through.
-   * 
+   *
    * @return  void  Nothing
    */
   public function indexAction()
@@ -40,9 +40,9 @@ class ExamplesController extends Controller
 
   /**
    * ExamplesController::viewAction()
-   * 
+   *
    * View the selected example, which basically renders the pChart chart.
-   * 
+   *
    * @return  void  Nothing
    */
   public function viewAction($number)
@@ -85,9 +85,9 @@ class ExamplesController extends Controller
 
   /**
    * ExamplesController::exampleAction()
-   * 
+   *
    * Render the pChart chart example or return a default error message.
-   * 
+   *
    * @return  void  Nothing
    */
   public function exampleAction($number)
@@ -101,7 +101,7 @@ class ExamplesController extends Controller
     {
       $chart = $this->$function();
     }
-    
+
     if (is_object($chart) && $chart instanceof pImage)
     {
       /* Capture output and return the response */
@@ -118,12 +118,12 @@ class ExamplesController extends Controller
       $font_path = __DIR__ . "/../Resources/fonts/Silkscreen.ttf";
       $string = 'pChart error';
       imagettftext($image, 50, 0, 10, 60, $white, $font_path, $string);
-       
+
       ob_start();
       imagepng($image);
       $response->setContent(ob_get_clean());
 
-      //Clear up memory 
+      //Clear up memory
       imagedestroy($image);
     }
 
@@ -132,17 +132,17 @@ class ExamplesController extends Controller
 
   /**
    * ExamplesController::test1()
-   * 
+   *
    * Example #1
-   * 
-   * @note example.addRandomValues
-   * 
+   *
+   * example.addRandomValues
+   *
    * @return  void  Nothing
    */
   protected function test1()
   {
     /* Create the pData object with some random values*/
-    $MyData = new pData();  
+    $MyData = new pData();
     $MyData->addRandomValues("Probe 1",array("Values"=>30,"Min"=>0,"Max"=>4));
     $MyData->addRandomValues("Probe 2",array("Values"=>30,"Min"=>6,"Max"=>10));
     $MyData->addRandomValues("Probe 3",array("Values"=>30,"Min"=>12,"Max"=>16));
@@ -164,7 +164,7 @@ class ExamplesController extends Controller
     /* Add a border to the picture */
     $myPicture->drawRectangle(0,0,699,229,array("R"=>0,"G"=>0,"B"=>0));
 
-    /* Write the picture title */ 
+    /* Write the picture title */
     $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/Silkscreen.ttf","FontSize"=>6));
     $myPicture->drawText(10,13,"addRandomValues() :: assess your scales",array("R"=>255,"G"=>255,"B"=>255));
 
@@ -191,11 +191,11 @@ class ExamplesController extends Controller
 
   /**
    * ExamplesController::test2()
-   * 
+   *
    * Example #2
-   * 
-   * @note example.barcode
-   * 
+   *
+   * example.barcode
+   *
    * @return  void  Nothing
    */
   protected function test2()
@@ -204,12 +204,12 @@ class ExamplesController extends Controller
     $myPicture = new pImage(600,310,NULL,TRUE);
 
     /* Draw the rounded box */
-    $myPicture->setShadow(TRUE,array("X"=>2,"Y"=>2,"R"=>0,"G"=>0,"B"=>0,"Alpha"=>30));  
+    $myPicture->setShadow(TRUE,array("X"=>2,"Y"=>2,"R"=>0,"G"=>0,"B"=>0,"Alpha"=>30));
     $Settings = array("R"=>255,"G"=>255,"B"=>255,"BorderR"=>0,"BorderG"=>0,"BorderB"=>0);
     $myPicture->drawRoundedFilledRectangle(10,10,590,300,10,$Settings);
 
     /* Draw the cell divisions */
-    $myPicture->setShadow(FALSE);  
+    $myPicture->setShadow(FALSE);
     $Settings = array("R"=>0,"G"=>0,"B"=>0);
     $myPicture->drawLine(10,110,590,110,$Settings);
     $myPicture->drawLine(200,10,200,110,$Settings);
@@ -221,60 +221,60 @@ class ExamplesController extends Controller
     $myPicture->drawLine(400,220,400,300,$Settings);
 
     /* Write the fields labels */
-    $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/Forgotte.ttf","FontSize"=>10)); 
+    $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/Forgotte.ttf","FontSize"=>10));
     $Settings = array("R"=>0,"G"=>0,"B"=>0,"Align"=>TEXT_ALIGN_TOPLEFT);
-    $myPicture->drawText(20,20,"FROM",$Settings); 
-    $myPicture->drawText(210,20,"TO",$Settings); 
-    $myPicture->drawText(20,120,"ACCT.\r\nNUMBER",$Settings); 
-    $myPicture->drawText(20,166,"QUANTITY",$Settings); 
-    $myPicture->drawText(230,166,"SHIPMENT CODE",$Settings); 
-    $myPicture->drawText(330,166,"SENDER CODE",$Settings); 
-    $myPicture->drawText(410,220,"MFG DATE",$Settings); 
-    $myPicture->drawText(410,260,"NET WEIGTH",$Settings); 
+    $myPicture->drawText(20,20,"FROM",$Settings);
+    $myPicture->drawText(210,20,"TO",$Settings);
+    $myPicture->drawText(20,120,"ACCT.\r\nNUMBER",$Settings);
+    $myPicture->drawText(20,166,"QUANTITY",$Settings);
+    $myPicture->drawText(230,166,"SHIPMENT CODE",$Settings);
+    $myPicture->drawText(330,166,"SENDER CODE",$Settings);
+    $myPicture->drawText(410,220,"MFG DATE",$Settings);
+    $myPicture->drawText(410,260,"NET WEIGTH",$Settings);
 
     /* Filling the fields values */
-    $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/Forgotte.ttf","FontSize"=>16)); 
-    $myPicture->drawText(70,20,"BEBEER INC\r\n342, MAIN STREET\r\n33000 BORDEAUX\r\nFRANCE",$Settings); 
-    $myPicture->drawText(250,20,"MUSTAFA'S BAR\r\n18, CAPITOL STREET\r\n31000 TOULOUSE\r\nFRANCE",$Settings); 
+    $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/Forgotte.ttf","FontSize"=>16));
+    $myPicture->drawText(70,20,"BEBEER INC\r\n342, MAIN STREET\r\n33000 BORDEAUX\r\nFRANCE",$Settings);
+    $myPicture->drawText(250,20,"MUSTAFA'S BAR\r\n18, CAPITOL STREET\r\n31000 TOULOUSE\r\nFRANCE",$Settings);
 
-    $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/Forgotte.ttf","FontSize"=>35)); 
-    $myPicture->drawText(100,120,"2342355552340",$Settings); 
+    $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/Forgotte.ttf","FontSize"=>35));
+    $myPicture->drawText(100,120,"2342355552340",$Settings);
 
-    $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/Forgotte.ttf","FontSize"=>20)); 
+    $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/Forgotte.ttf","FontSize"=>20));
     $Settings = array("R"=>0,"G"=>0,"B"=>0,"Align"=>TEXT_ALIGN_TOPRIGHT);
-    $myPicture->drawText(210,180,"75 CANS",$Settings); 
-    $myPicture->drawText(310,180,"TLSE",$Settings); 
-    $myPicture->drawText(580,180,"WAREHOUSE#SLOT#B15",$Settings); 
+    $myPicture->drawText(210,180,"75 CANS",$Settings);
+    $myPicture->drawText(310,180,"TLSE",$Settings);
+    $myPicture->drawText(580,180,"WAREHOUSE#SLOT#B15",$Settings);
 
     $Settings = array("R"=>0,"G"=>0,"B"=>0,"Align"=>TEXT_ALIGN_TOPLEFT);
-    $myPicture->drawText(410,236,"06/06/2010",$Settings); 
-    $myPicture->drawText(410,276,"12.340 Kg",$Settings); 
+    $myPicture->drawText(410,236,"06/06/2010",$Settings);
+    $myPicture->drawText(410,276,"12.340 Kg",$Settings);
 
-    /* Create the barcode 39 object */ 
-    $Barcode39 = new pBarcode39(__DIR__ . "/../Resources/"); 
-    $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/pf_arma_five.ttf","FontSize"=>6)); 
-    $Settings = array("ShowLegend"=>TRUE,"Height"=>55,"DrawArea"=>TRUE,"DrawArea"=>FALSE); 
-    $Barcode39->draw($myPicture,"12250000234502",30,220,$Settings); 
+    /* Create the barcode 39 object */
+    $Barcode39 = new pBarcode39(__DIR__ . "/../Resources/");
+    $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/pf_arma_five.ttf","FontSize"=>6));
+    $Settings = array("ShowLegend"=>TRUE,"Height"=>55,"DrawArea"=>TRUE,"DrawArea"=>FALSE);
+    $Barcode39->draw($myPicture,"12250000234502",30,220,$Settings);
 
-    $Settings = array("ShowLegend"=>TRUE,"Height"=>14,"DrawArea"=>TRUE,"DrawArea"=>FALSE); 
-    $Barcode39->draw($myPicture,"75 cans",260,220,$Settings); 
-    $Barcode39->draw($myPicture,"06062010",260,260,$Settings); 
+    $Settings = array("ShowLegend"=>TRUE,"Height"=>14,"DrawArea"=>TRUE,"DrawArea"=>FALSE);
+    $Barcode39->draw($myPicture,"75 cans",260,220,$Settings);
+    $Barcode39->draw($myPicture,"06062010",260,260,$Settings);
 
-    /* Create the barcode 128 object */ 
-    $Barcode128 = new pBarcode128(__DIR__ . "/../Resources/"); 
-    $Settings = array("ShowLegend"=>TRUE,"Height"=>65,"DrawArea"=>TRUE,"DrawArea"=>FALSE); 
-    $Barcode128->draw($myPicture,"TLSE",450,25,$Settings); 
+    /* Create the barcode 128 object */
+    $Barcode128 = new pBarcode128(__DIR__ . "/../Resources/");
+    $Settings = array("ShowLegend"=>TRUE,"Height"=>65,"DrawArea"=>TRUE,"DrawArea"=>FALSE);
+    $Barcode128->draw($myPicture,"TLSE",450,25,$Settings);
 
     return $myPicture;
   }
 
   /**
    * ExamplesController::test3()
-   * 
+   *
    * Example #3
-   * 
-   * @note example.barcode39
-   * 
+   *
+   * example.barcode39
+   *
    * @return  void  Nothing
    */
   protected function test3()
@@ -321,11 +321,11 @@ class ExamplesController extends Controller
 
   /**
    * ExamplesController::test4()
-   * 
+   *
    * Example #4
-   * 
-   * @note example.barcode128
-   * 
+   *
+   * example.barcode128
+   *
    * @return  void  Nothing
    */
   protected function test4()
@@ -372,22 +372,22 @@ class ExamplesController extends Controller
 
   /**
    * ExamplesController::test5()
-   * 
+   *
    * Example #5
-   * 
-   * @note example.basic
-   * 
+   *
+   * example.basic
+   *
    * @return  void  Nothing
    */
   protected function test5()
   {
-    /* Create your dataset object */ 
-    $myData = new pData(); 
+    /* Create your dataset object */
+    $myData = new pData();
 
-    /* Add data in your dataset */ 
+    /* Add data in your dataset */
     $myData->addPoints(array(1,3,4,3,5));
 
-    /* Create a pChart object and associate your dataset */ 
+    /* Create a pChart object and associate your dataset */
     $myPicture = new pImage(700,230,$myData);
 
     /* Choose a nice font */
@@ -396,10 +396,10 @@ class ExamplesController extends Controller
     /* Define the boundaries of the graph area */
     $myPicture->setGraphArea(60,40,670,190);
 
-    /* Draw the scale, keep everything automatic */ 
+    /* Draw the scale, keep everything automatic */
     $myPicture->drawScale();
 
-    /* Draw the scale, keep everything automatic */ 
+    /* Draw the scale, keep everything automatic */
     $myPicture->drawSplineChart();
 
     return $myPicture;
@@ -407,17 +407,17 @@ class ExamplesController extends Controller
 
   /**
    * ExamplesController::test6()
-   * 
+   *
    * Example #6
-   * 
-   * @note example.Combo.area.lines
-   * 
+   *
+   * example.Combo.area.lines
+   *
    * @return  void  Nothing
    */
   protected function test6()
   {
     /* Create and populate the pData object */
-    $MyData = new pData();  
+    $MyData = new pData();
     $MyData->addPoints(array(4,2,10,12,8,3),"Probe 1");
     $MyData->addPoints(array(3,12,15,8,5,5),"Probe 2");
     $MyData->setSerieTicks("Probe 2",4);
@@ -432,18 +432,18 @@ class ExamplesController extends Controller
     /* Turn of Antialiasing */
     $myPicture->Antialias = FALSE;
 
-    /* Draw the background */ 
+    /* Draw the background */
     $Settings = array("R"=>170, "G"=>183, "B"=>87, "Dash"=>1, "DashR"=>190, "DashG"=>203, "DashB"=>107);
-    $myPicture->drawFilledRectangle(0,0,700,230,$Settings); 
+    $myPicture->drawFilledRectangle(0,0,700,230,$Settings);
 
-    /* Overlay with a gradient */ 
+    /* Overlay with a gradient */
     $Settings = array("StartR"=>219, "StartG"=>231, "StartB"=>139, "EndR"=>1, "EndG"=>138, "EndB"=>68, "Alpha"=>50);
-    $myPicture->drawGradientArea(0,0,700,230,DIRECTION_VERTICAL,$Settings); 
+    $myPicture->drawGradientArea(0,0,700,230,DIRECTION_VERTICAL,$Settings);
 
     /* Add a border to the picture */
     $myPicture->drawRectangle(0,0,699,229,array("R"=>0,"G"=>0,"B"=>0));
 
-    /* Write the chart title */ 
+    /* Write the chart title */
     $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/Forgotte.ttf","FontSize"=>11));
     $myPicture->drawText(150,35,"Average temperature",array("FontSize"=>20,"Align"=>TEXT_ALIGN_BOTTOMMIDDLE));
 
@@ -479,17 +479,17 @@ class ExamplesController extends Controller
 
   /**
    * ExamplesController::test7()
-   * 
+   *
    * Example #7
-   * 
-   * @note example.Combo
-   * 
+   *
+   * example.Combo
+   *
    * @return  void  Nothing
    */
   protected function test7()
   {
     /* Create the pData object with some random values*/
-    $MyData = new pData(); 
+    $MyData = new pData();
     $MyData->addPoints(array(30,24,32),"This year");
     $MyData->addPoints(array(28,20,27),"Last year");
     $MyData->setSerieTicks("Last year",4);
@@ -514,7 +514,7 @@ class ExamplesController extends Controller
     /* Add a border to the picture */
     $myPicture->drawRectangle(0,0,699,229,array("R"=>0,"G"=>0,"B"=>0));
 
-    /* Write the picture title */ 
+    /* Write the picture title */
     $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/Silkscreen.ttf","FontSize"=>6));
     $myPicture->drawText(10,13,"Chart title",array("R"=>255,"G"=>255,"B"=>255));
 
@@ -558,18 +558,18 @@ class ExamplesController extends Controller
 
   /**
    * ExamplesController::test8()
-   * 
+   *
    * Example #8
-   * 
-   * @note example.draw2DPie
-   * 
+   *
+   * example.draw2DPie
+   *
    * @return  void  Nothing
    */
   protected function test8()
   {
     /* Create and populate the pData object */
-    $MyData = new pData();   
-    $MyData->addPoints(array(40,60,15,10,6,4),"ScoreA");  
+    $MyData = new pData();
+    $MyData->addPoints(array(40,60,15,10,6,4),"ScoreA");
     $MyData->setSerieDescription("ScoreA","Application A");
 
     /* Define the absissa serie */
@@ -591,26 +591,26 @@ class ExamplesController extends Controller
     /* Add a border to the picture */
     $myPicture->drawRectangle(0,0,699,229,array("R"=>0,"G"=>0,"B"=>0));
 
-    /* Write the picture title */ 
+    /* Write the picture title */
     $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/Silkscreen.ttf","FontSize"=>6));
     $myPicture->drawText(10,13,"pPie - Draw 2D pie charts",array("R"=>255,"G"=>255,"B"=>255));
 
-    /* Set the default font properties */ 
+    /* Set the default font properties */
     $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/Forgotte.ttf","FontSize"=>10,"R"=>80,"G"=>80,"B"=>80));
 
-    /* Enable shadow computing */ 
+    /* Enable shadow computing */
     $myPicture->setShadow(TRUE,array("X"=>2,"Y"=>2,"R"=>0,"G"=>0,"B"=>0,"Alpha"=>50));
 
-    /* Create the pPie object */ 
+    /* Create the pPie object */
     $PieChart = new pPie($myPicture,$MyData);
 
-    /* Draw a simple pie chart */ 
+    /* Draw a simple pie chart */
     $PieChart->draw2DPie(120,125,array("SecondPass"=>FALSE));
 
-    /* Draw an AA pie chart */ 
+    /* Draw an AA pie chart */
     $PieChart->draw2DPie(340,125,array("DrawLabels"=>TRUE,"LabelStacked"=>TRUE,"Border"=>TRUE));
 
-    /* Draw a splitted pie chart */ 
+    /* Draw a splitted pie chart */
     $PieChart->draw2DPie(560,125,array("WriteValues"=>PIE_VALUE_PERCENTAGE,"DataGapAngle"=>10,"DataGapRadius"=>6,"Border"=>TRUE,"BorderR"=>255,"BorderG"=>255,"BorderB"=>255));
 
     /* Write the legend */
@@ -619,7 +619,7 @@ class ExamplesController extends Controller
     $myPicture->drawText(120,200,"Single AA pass",array("DrawBox"=>TRUE,"BoxRounded"=>TRUE,"R"=>0,"G"=>0,"B"=>0,"Align"=>TEXT_ALIGN_TOPMIDDLE));
     $myPicture->drawText(440,200,"Extended AA pass / Splitted",array("DrawBox"=>TRUE,"BoxRounded"=>TRUE,"R"=>0,"G"=>0,"B"=>0,"Align"=>TEXT_ALIGN_TOPMIDDLE));
 
-    /* Write the legend box */ 
+    /* Write the legend box */
     $myPicture->setFontProperties(array("FontName"=>__DIR__ . "/../Resources/fonts/Silkscreen.ttf","FontSize"=>6,"R"=>255,"G"=>255,"B"=>255));
     $PieChart->drawPieLegend(380,8,array("Style"=>LEGEND_NOBORDER,"Mode"=>LEGEND_HORIZONTAL));
 
